83bcefd5d4468766f7f730a2c8c4941c26a0e48d
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2020-04-05 00:48:52 +0200

    dasf

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const cool = require('cool-ascii-faces')
 
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
